d1d9f0bc43a5d17612764fe54d293e349f1c2870
10cheon00 <10cheon00@gmail.com>
2023-09-06 15:51:36 +0900

    feat: apply dark mode theme on syntax highlighting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,14 +4,11 @@ import "@fontsource/merriweather"
 // normalize CSS across browsers
 import "./src/normalize.css"
 // Highlighting for code blocks
-import "prismjs/themes/prism.css"
-import "./src/styles/prism-ghlight.css"
+// import "prismjs/themes/prism.css"
+import "./src/styles/prism-ghtheme.css"
+import "./src/styles/prism-ghcolor.css"
 // Highlighting for math syntax
 import "katex/dist/katex.min.css";
 
 // custom CSS styles
 import "./src/style.css"
-
-if (document.body.classList.contains("dark")) {
-    // require("./src/styles/prism-ghdark.css");
-}
