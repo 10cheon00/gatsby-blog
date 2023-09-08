425dde1f8a99e929707fd51bc67f827c27153f6d
10cheon00 <10cheon00@gmail.com>
2023-09-08 13:31:38 +0900

    feat: apply theme toggling on utterances

--- src/components/theme-toggler.js
+++ src/components/theme-toggler.js
@@ -1,15 +1,31 @@
 import * as React from "react"
-import { ThemeToggler as DarkModeThemeToggler} from "gatsby-plugin-dark-mode"
+import { ThemeToggler as DarkModeThemeToggler } from "gatsby-plugin-dark-mode"
 
 class ThemeToggler extends React.Component {
   render() {
+    const changeUtteranceTheme = () => {
+      const utterancesEl = document.querySelector(".utterances-frame")
+      if (utterancesEl) {
+        const theme = document.body.classList.contains("dark")
+          ? "github-dark"
+          : "github-light"
+        const message = {
+          type: "set-theme",
+          theme: theme,
+        }
+        utterancesEl.contentWindow.postMessage(message, "https://utteranc.es")
+      }
+    }
     return (
       <DarkModeThemeToggler>
         {({ theme, toggleTheme }) => (
           <label className="theme-toggler">
             <input
               type="checkbox"
-              onChange={e => toggleTheme(e.target.checked ? "dark" : "light")}
+              onChange={e => {
+                toggleTheme(e.target.checked ? "dark" : "light")
+                changeUtteranceTheme()
+              }}
               checked={theme === "dark"}
             />{" "}
             <span>Dark mode</span>
--- src/components/utterances.js
+++ src/components/utterances.js
@@ -1,22 +1,27 @@
-import * as React from 'react'
+import * as React from "react"
 
 class Utterances extends React.Component {
   constructor(props) {
     super(props)
 
     this.commentsEl = React.createRef()
-    this.state = { status: 'pending' }
+    this.state = { status: "pending" }
   }
   componentDidMount() {
-    const scriptEl = document.createElement('script')
-    scriptEl.onload = () => this.setState({ status: 'success' })
-    scriptEl.onerror = () => this.setState({ status: 'failed' })
+    const scriptEl = document.createElement("script")
+    scriptEl.onload = () => this.setState({ status: "success" })
+    scriptEl.onerror = () => this.setState({ status: "failed" })
     scriptEl.async = true
-    scriptEl.src = 'https://utteranc.es/client.js'
-    scriptEl.setAttribute('repo', '10cheon00/10cheon00.github.io')
-    scriptEl.setAttribute('issue-term', 'title')
-    scriptEl.setAttribute('theme', 'github-light')
-    scriptEl.setAttribute('crossorigin', 'anonymous')
+    scriptEl.src = "https://utteranc.es/client.js"
+    scriptEl.setAttribute("repo", "10cheon00/10cheon00.github.io")
+    scriptEl.setAttribute("issue-term", "title")
+
+    // initialize on first access.
+    const theme = document.body.classList.contains("dark")
+      ? "github-dark"
+      : "github-light"
+    scriptEl.setAttribute("theme", theme)
+    scriptEl.setAttribute("crossorigin", "anonymous")
     this.commentsEl.current.appendChild(scriptEl)
   }
 
@@ -25,8 +30,8 @@ class Utterances extends React.Component {
 
     return (
       <div className="comments-wrapper">
-        {status === 'failed' && <div>Error. Please try again.</div>}
-        {status === 'pending' && <div>Loading script...</div>}
+        {status === "failed" && <div>Error. Please try again.</div>}
+        {status === "pending" && <div>Loading script...</div>}
         <div ref={this.commentsEl} />
       </div>
     )
