1956c5fd7f76de26fe3febdf5316f2141e289292
10cheon00 <10cheon00@gmail.com>
2023-11-08 13:42:53 +0900

    refactor: move nav bar codes into top-bar.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,40 +1,15 @@
 import * as React from "react"
 
-import Bio from "../components/bio"
-import { Link } from "gatsby"
-import { ThemeToggler } from "./theme-toggler"
-import { FaHouse } from "react-icons/fa6"
+import TopBar from "./top-bar"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
-  // let header
-
-  // if (isRootPath) {
-  //   header = (
-  //     <h1 className="main-heading">
-  //       <Link to="/">{title}</Link>
-  //     </h1>
-  //   )
-  // } else {
-  //   header = (
-  //     <Link className="header-link-home" to="/">
-  //       <FaHouse className="home-icon"></FaHouse>
-  //       {title}
-  //     </Link>
-  //   )
-  // }
 
   return (
     <div>
-      <div className="nav-bar">
-        <nav className="nav">
-          <h1 className="main-heading">
-            <Link to="/">{title}</Link>
-          </h1>
-          <ThemeToggler />
-        </nav>
-      </div>
+      <TopBar title={title}/>
+      
       <div className="global-wrapper" data-is-root-path={isRootPath}>
         {/* <Bio /> */}
         <main>{children}</main>
---
+++ src/components/top-bar.js
@@ -0,0 +1,70 @@
+/**
+ * Bio component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
+ */
+
+import * as React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import { ThemeToggler } from "./theme-toggler"
+import { FaGithub } from "react-icons/fa6"
+
+const TopBar = ({ title }) => {
+  const data = useStaticQuery(graphql`
+    query BioQuery {
+      site {
+        siteMetadata {
+          author {
+            name
+            summary
+          }
+          social {
+            github
+          }
+        }
+      }
+    }
+    `)
+    
+  // Set these values by editing "siteMetadata" in gatsby-config.js
+  // const author = data.site.siteMetadata?.author
+  const social = data.site.siteMetadata?.social
+
+  return (
+    <div className="top-bar">
+      <Link className="home-link" to="/">
+        <StaticImage
+          className="bio-avatar"
+          layout="fixed"
+          formats={["auto", "webp", "avif"]}
+          src="../images/profile-icon.png"
+          width={50}
+          height={50}
+          quality={95}
+          alt="Profile picture"
+        />
+        <h1 className="main-heading">{title}</h1>
+      </Link>
+      <div className="top-bar-links">
+        <Link to="/tags">
+          <h1>Tags</h1>
+        </Link>
+        <Link to="/tags">
+          <h1>Tags</h1>
+        </Link>
+        {/* add custom links */}
+      </div>
+      <div className="top-bar-right">
+        <a className="bio-icons" href={`${social?.github || ``}`}>
+          <FaGithub size="1.5rem"></FaGithub>
+        </a>
+        <ThemeToggler/>
+      </div>
+    </div>
+  )
+}
+
+export default TopBar
