b06cd665629f849e08cc680f5709f1c4337dac29
10cheon00 <10cheon00@gmail.com>
2024-01-15 18:17:08 +0900

    fix: change anchor position
    Anchor is located just below from top bar. Whenever user click on content
    table or link tag, page will be moved to equivalent header and its
    position is near to top bar.

--- src/components/table-of-contents.js
+++ src/components/table-of-contents.js
@@ -3,32 +3,6 @@ import * as React from "react"
 class TableOfContents extends React.Component {
   componentDidMount() {
     window.addEventListener(`scroll`, this.updateTableOfContents)
-
-    Array.from(document.querySelectorAll(`div.table-of-contents a`)).forEach(
-      (link, linkIndex) => {
-        link.addEventListener(`click`, event => {
-          event.preventDefault()
-
-          const target = Array.from(
-            document.querySelectorAll(`
-            section[itemProp="articleBody"] h1,
-            section[itemProp="articleBody"] h2,
-            section[itemProp="articleBody"] h3
-          `)
-          ).find((header, headerIndex) => {
-            return (
-              header.textContent === link.textContent &&
-              headerIndex === linkIndex
-            )
-          })
-
-          window.scrollTo({
-            top: target.offsetTop - window.innerHeight / 2 + 1,
-            behavior: `instant`,
-          })
-        })
-      }
-    )
   }
 
   updateTableOfContents() {
@@ -38,14 +12,14 @@ class TableOfContents extends React.Component {
       section[itemProp="articleBody"] h3
     `)
     const links = document.querySelectorAll(`div.table-of-contents a`)
-    const halfOfWindowHeight = window.innerHeight / 2
+    const topBarHeight = parseInt(
+      getComputedStyle(document.querySelector(`.top-bar`)).height
+    )
 
-    const currentHeader = Array.from(headers)
-      .reverse()
-      .find(e => {
-        const pos = e.getBoundingClientRect()
-        return pos.y < halfOfWindowHeight
-      })
+    const currentHeader = Array.from(headers).find(e => {
+      const pos = e.getBoundingClientRect()
+      return pos.y > topBarHeight
+    })
 
     links.forEach(link => {
       const decodedId = decodeURI(new URL(link.href).hash).slice(1)
