c086f088aca936d5028db92c0370696c61e95765
10cheon00 <10cheon00@gmail.com>
2024-01-30 02:58:54 +0900

    feat: add style on sidebar

--- src/components/category.js
+++ src/components/category.js
@@ -1,15 +1,26 @@
 import * as React from "react"
 
-const Category = ({ data }) => {
+const Category = ({ data, subTreeCount = 0 }) => {
   const generateSubCategory = group =>
-    group && group.length > 0 ? <Category data={group} className="subcategory" /> : null
+    group && group.length > 0 ? (
+      <Category
+        data={group}
+        subTreeCount={subTreeCount + 1}
+        className="subcategory"
+      />
+    ) : null
+
+  const indent = "|\u00a0\u00a0\u00a0".repeat(subTreeCount)
+  const generateTreePrefix = index =>
+    `${indent}${index !== data.length - 1 ? "+-- " : "`-- "}`
 
   return (
     <ul>
-      {data.map(({ group, fieldValue, totalCount }) => {
+      {data.map(({ group, fieldValue, totalCount }, index) => {
         return (
           <li key={fieldValue} className="category-link">
-            <span>{fieldValue}</span>&nbsp;<span>({totalCount})</span>
+            <span>{`${generateTreePrefix(index)}${fieldValue}`}</span>
+            &nbsp;<span>({totalCount})</span>
             {generateSubCategory(group)}
           </li>
         )
